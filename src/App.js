82527611b5ac5ee0105392feebfde1dbddac6634
The-Unknown-Developer <theunknowndevloper@gmail.com>
2023-05-30 23:57:10 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    
+    const [mode, setMode] = React.useState( {lm: true})
+    
+    function handleMode(){
+        return setMode((prevState) => {
+  
+            return !prevState
+        })
+       
+    }
+
+    return (
+        <div className="container">
+            <Navbar lightMode={mode} toggleDarkMode={handleMode}/>
+            <Main lightMode={mode} />
+        </div>
+    )
 }
-
-export default App;
